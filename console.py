fc085b67f3d4175481b1769d39301a1ad5523100
Scott Reoch <scottreoch10@hotmail.co.uk>
2021-12-01 00:15:20 +0000

    finished MVP

--- console.py
+++ console.py
@@ -1,3 +1,21 @@
 import pdb
 from models.album import Album
 from models.artist import Artist
+
+import repositories.album_repository as album_repository
+import repositories.artist_repository as artist_repository
+
+album_repository.delete_all()
+artist_repository.delete_all()
+
+artist_1 = Artist("Slowdive")
+artist_repository.save(artist_1)
+artist_2 = Artist("Deftones")
+artist_repository.save(artist_2)
+
+album = Album("Souvlaki", "shoegaze", artist_1)
+album_repository.save(album)
+
+artist_repository.select_all()
+
+pdb.set_trace()
--- repositories/album_repository.py
+++ repositories/album_repository.py
@@ -2,4 +2,39 @@ from db.run_sql import run_sql
 
 from models.album import Album
 from models.artist import Artist
+import repositories.artist_repository as artist_repository
 
+def save(album):
+    sql = "INSERT INTO albums (title, genre, artist_id) VALUES (%s, %s, %s) RETURNING *"
+    values = [album.title, album.genre, album.artist.id]
+    results = run_sql(sql, values)
+    id = results[0]["id"]
+    album.id = id
+    return album
+
+def delete_all():
+    sql = "DELETE  FROM albums"
+    run_sql(sql)
+
+def select(id):
+    album = None
+    sql = "SELECT * FROM albums WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        artist = artist_repository.select(result['user_id'])
+        album = Album(result['title'], result['genre'], artist)
+    return album
+
+def select_all():
+    albums = []
+
+    sql = "SELECT * FROM albums"
+    results = run_sql(sql)
+
+    for row in results:
+        artist = artist_repository.select(row['artist_id'])
+        album = Album(row['title'], row['genre'], artist)
+        albums.append(album)
+    return albums
